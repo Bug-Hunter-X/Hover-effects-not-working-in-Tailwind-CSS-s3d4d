55e96cbd7d9986c319adc421335f795d6e7e1814
BugHunter-X <askzenai@gmail.com>
2025-01-04 21:00:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,6 @@
+```javascript
+// Solution: Add !important flag to the hover styles
+div className="bg-red-500 hover:bg-red-700 !important text-white font-bold py-2 px-4 rounded">
+  Hello world!
+</div>
+```
